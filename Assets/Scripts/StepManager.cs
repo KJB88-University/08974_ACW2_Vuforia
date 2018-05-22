8448dba3b7b338b0dbf044b54da58390e07c526f
563794 <K.J.Barr@hull.ac.uk>
2018-05-22 21:59:42 +0100

    Init. commit

---
+++ Assets/Scripts/StepManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepManager : MonoBehaviour
+{
+    /*
+    [SerializeField]
+    GameObject[] firstObjects;
+
+    [SerializeField]
+    GameObject[] secondObjects;
+    */
+
+    int currentStep = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void NextStep()
+    {
+        currentStep++;
+    }
+
+    void ResetSteps()
+    {
+        currentStep = 0;
+    }
+}
---
+++ Assets/Scripts/TeaObjectAttributes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OBJECTTYPE
+{
+    DRAGGABLE,
+    SELECTABLE,
+    QR_LINKED,
+    STATIC
+}
+public class TeaObjectAttributes : MonoBehaviour
+{
+    // Name to be displayed
+    public string Name;
+
+    // How does this object interact with the world?
+    public OBJECTTYPE objectType;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
